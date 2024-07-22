4dd6afcec4431beec267f23051b9895a3c28b584
frehburg <fil.rehburg@gmail.com>
2024-07-22 17:02:59 +0200

    add constructor

--- src/main/java/org/monarchinitiative/hpotools/analysis/simhpo/ProportionalRandomSelection.java
+++ src/main/java/org/monarchinitiative/hpotools/analysis/simhpo/ProportionalRandomSelection.java
@@ -1,5 +1,16 @@
-package org.monarchinitiative.hpotools.analysis.simhpo;public class ProportionalRandomSelection {
-}
+package org.monarchinitiative.hpotools.analysis.simhpo;
+
+import java.util.Random;
+
 public class ProportionalRandomSelection<E> {
-}
-}
+    E[] elements;
+    double[] probabilities;
+    double[] cumulativeProbabilities;
+    Random random;
+
+    public ProportionalRandomSelection(E[] elements, double[] probabilities, Random random) {
+        this.elements = elements;
+        this.probabilities = probabilities;
+        this.random = random;
+        this.cumulativeProbabilities = cumulativeProbabilities();
+    }
