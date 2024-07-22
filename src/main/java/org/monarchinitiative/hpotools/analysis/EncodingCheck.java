fd0c1cd3d8230466c62e642ef42a82c6006d6f9a
Peter Robinson <peter.robinson@jax.org>
2024-07-22 10:12:57 +0200

    improving output of encoding check

--- src/main/java/org/monarchinitiative/hpotools/analysis/EncodingCheck.java
+++ src/main/java/org/monarchinitiative/hpotools/analysis/EncodingCheck.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,37 +28,57 @@ public class EncodingCheck {
     public void checkEncoding() {
 
         int i = 0;
+        int encodingErrors = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(hpoOwlFile))){
             String line;
             String hp_term = "";
+
             while ((line = br.readLine()) != null) {
                 Matcher m = HPO_TERM_PATTERN.matcher(line);
                 if (m.find()) {
                     hp_term = m.group();
                 }
-                checkLine(line, ++i, hp_term);
+                encodingErrors += checkLine(line, ++i, hp_term);
             }
         } catch (IOException e) {
             LOGGER.error("Could not read hpo owl file", e);
         }
+        if (encodingErrors == 0) {
+            LOGGER.info("[INFO] No encoding errors detected");
+            System.out.println("No encoding errors detected");
+        }  else {
+            LOGGER.error("Encoding errors detected: " + encodingErrors);
+        }
     }
 
-    public static void checkLine(String line, int lineno, String previous) {
+
+    /** The following are flagged as potential errors by the checkLine function,
+     * but do not actually cause an error in our pipeline and this can be skipped.
+     */
+    private static Set<Character> acceptableChars = Set.of('ö', 'ü', 'à', 'é', 'ï');
+
+    public static int checkLine(String line, int lineno, String previous) {
         byte[] bytes = line.getBytes(StandardCharsets.ISO_8859_1);
         String decodedLine = new String(bytes);
         if (! line.equals(decodedLine)) {
             for (int i=0; i< line.length();i++) {
-                if (line.charAt(i) != decodedLine.charAt(i)) {
+                char offendingChar = line.charAt(i);
+                if (acceptableChars.contains(offendingChar)) {
+                    continue;
+                }
+                if (offendingChar != decodedLine.charAt(i)) {
                     System.out.println(previous);
                     System.out.printf("L.%d:Pos:%d: ", lineno, i);
                     int b = Math.max(0, i-20);
                     int e = Math.min(line.length(), i+20);
                     String ss1 = line.substring(b, i);
                     String ss2 = line.substring(i+1, e);
-                    System.out.printf("%s{%c}%sc\n\n", ss1, line.charAt(i), ss2);
+                    System.out.printf("%s{%c}%sc\n\n", ss1, offendingChar, ss2);
+                    return 1;
                 }
             }
         }
+        return 0;
     }
 
 
