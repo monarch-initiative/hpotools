1928896986aa86e13038e80ac01be995ced6455a
frehburg <fil.rehburg@gmail.com>
2024-07-24 12:27:20 +0200

    remove unnecessary code chunk

--- src/main/java/org/monarchinitiative/hpotools/analysis/simhpo/SimulatedHpoDiseaseGenerator.java
+++ src/main/java/org/monarchinitiative/hpotools/analysis/simhpo/SimulatedHpoDiseaseGenerator.java
@@ -101,10 +101,6 @@ public class SimulatedHpoDiseaseGenerator {
 
             System.out.println("Probabilities: " + Arrays.toString(probabilities));
 
-            // Add n_terms HPO terms to the phenopacket
-            var temp = disease.phenotypicAbnormalities(); // TODO @pnrobinson: all methods regarding phenotypic abnormalities are deprecated, what should be used instead?
-
-
             // Add annotations to the phenopacket
             List<HpoDiseaseAnnotation> annotations = (List<HpoDiseaseAnnotation>) disease.annotations();
             ProportionalRandomSelection<HpoDiseaseAnnotation> prs = new ProportionalRandomSelection<>(annotations, probabilities, random);
